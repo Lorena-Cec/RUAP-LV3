9b3793d15844ef1a9bf7ff1563464da5d38b32e2
Lorena-Cec <137172635+Lorena-Cec@users.noreply.github.com>
2023-11-24 18:12:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,6 @@ register.</p>
  <?php
 // DB connection info
 // TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved
-earlier from the Azure Portal. $host = "value of Data Source";
 $host = "lcec1-server.mysql.database.azure.com";
 $user = "wmhgkcnfgw";
 $pwd = "7Z125YA78T76QG8Q$";
@@ -110,17 +109,6 @@ echo "<h3>No one is currently registered.</h3>";
 }
  else
 {
-U terminalu idite na putanju mape vaše aplikacije i upišite sljedeću naredbu:
-Sada možete u pregledniku otići na adresu http://localhost:8000/ kako biste testirali vašu
-aplikaciju.
-6. Objavljivanje vaše aplikacije
-Nakon što ste testirali vašu aplikaciju lokalno, možete ju objaviti na Web Apps-u korištenjem
-Git-a. Prvo ćete inicijalizirati svoj lokalni Git repozitorij ili klonirati novi repozitorij koji ste
-kreirali na GitHub-u, te potom objaviti aplikaciju.
-1. Kako biste povezali svoj lokalni Git repozitorij s Azure Web App-om, potrebno je prvo
-definirati deployment opciju na Azure upravljačkoj konzoli vašeg Web App servisa.
-Odaberite Local Git Repository ili GitHub ukoliko ste se odlučili koristiti GitHub
-udaljeni repozitorij.
 echo "Insert Failed";
 }
 }
